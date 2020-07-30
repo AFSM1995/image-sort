509d8fa99a6da5e1d7a45633514db7265f5dbaf2
Alvaro Santillan <afsm1995@live.com>
2020-07-30 17:20:27 -0600

    Cleaned Up Sorted Square Fix.

--- Sort/Sort/Model/Sorting Algorithms/BubbleSort.swift
+++ Sort/Sort/Model/Sorting Algorithms/BubbleSort.swift
@@ -23,24 +23,23 @@ class BubbleSort {
     }
     
     func bubbleSort(gameboard: [SkNodeAndLocation]) -> [[SkNodeLocationAndColor]] {
-        let duplicateGameboard = gameboard
         var isSorted = false
         
-        var tempStructure: [ColorAndLocation] = []
+        var tempStructure: [UIColor] = []
         for i in scene.gameBoard {
-            tempStructure.append(ColorAndLocation(color: i.square.fillColor, location: i.location))
+            tempStructure.append(i.square.fillColor)
         }
 
         while (!isSorted) {
             isSorted = true
             for i in 0...((scene!.gameBoard.count)-scene.columnCount-3) {
-                if duplicateGameboard[i].location.x != 0 && duplicateGameboard[i].location.x != (scene.rowCount - 1) {
-                    if duplicateGameboard[i].location.y != 0 && duplicateGameboard[i].location.y != (scene.columnCount - 1) {
+                if gameboard[i].location.x != 0 && gameboard[i].location.x != (scene.rowCount - 1) {
+                    if gameboard[i].location.y != 0 && gameboard[i].location.y != (scene.columnCount - 1) {
                         var ii = i+1
 
                         var validNextSquare = false
                         while validNextSquare == false {
-                            if duplicateGameboard[ii].location.y == (scene.columnCount - 1) || duplicateGameboard[ii].location.y == 0 {
+                            if gameboard[ii].location.y == (scene.columnCount - 1) || gameboard[ii].location.y == 0 {
                                 ii += 1
                             } else {
                                 validNextSquare = true
@@ -56,21 +55,21 @@ class BubbleSort {
                         var blueTwo: CGFloat = 0
                         var alphaTwo: CGFloat = 0
 
-                        duplicateGameboard[i].square.fillColor.getRed(&redOne, green: &greenOne, blue: &blueOne, alpha: &alphaOne)
-                        duplicateGameboard[ii].square.fillColor.getRed(&redTwo, green: &greenTwo, blue: &blueTwo, alpha: &alphaTwo)
+                        gameboard[i].square.fillColor.getRed(&redOne, green: &greenOne, blue: &blueOne, alpha: &alphaOne)
+                        gameboard[ii].square.fillColor.getRed(&redTwo, green: &greenTwo, blue: &blueTwo, alpha: &alphaTwo)
 
 
                         if alphaOne < alphaTwo {
-                            duplicateGameboard[i].square.fillColor = UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo)
-                            duplicateGameboard[ii].square.fillColor = UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne)
+                            gameboard[i].square.fillColor = UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo)
+                            gameboard[ii].square.fillColor = UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne)
 
-                            let tempi = SkNodeLocationAndColor(square: duplicateGameboard[i].square, location: Tuple(x: duplicateGameboard[i].location.y, y: duplicateGameboard[i].location.x), color: UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo))
-                            let tempii = SkNodeLocationAndColor(square: duplicateGameboard[ii].square, location: Tuple(x: duplicateGameboard[ii].location.y, y: duplicateGameboard[ii].location.x), color: UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne))
+                            let tempi = SkNodeLocationAndColor(square: gameboard[i].square, location: Tuple(x: gameboard[i].location.y, y: gameboard[i].location.x), color: UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo))
+                            let tempii = SkNodeLocationAndColor(square: gameboard[ii].square, location: Tuple(x: gameboard[ii].location.y, y: gameboard[ii].location.x), color: UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne))
                             swapSquareAndColor.append([tempi, tempii])
 
                             isSorted = false
                         } else {
-                            let tempi = SkNodeLocationAndColor(square: duplicateGameboard[i].square, location: Tuple(x: duplicateGameboard[i].location.y, y: duplicateGameboard[i].location.x), color: UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo))
+                            let tempi = SkNodeLocationAndColor(square: gameboard[i].square, location: Tuple(x: gameboard[i].location.y, y: gameboard[i].location.x), color: UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo))
                             swapSquareAndColor.append([tempi])
                         }
                     }
@@ -79,11 +78,7 @@ class BubbleSort {
         }
         
         for (index, i) in (scene.gameBoard).enumerated() {
-            i.square.fillColor = tempStructure[index].color
-        }
-        
-        for i in gameboard {
-            print(i.square.fillColor)
+            i.square.fillColor = tempStructure[index]
         }
         
         return swapSquareAndColor
