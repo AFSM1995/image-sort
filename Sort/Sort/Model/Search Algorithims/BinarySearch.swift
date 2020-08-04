cf5d263eccf812f24dcbbc85590882b9f1be5668
Alvaro Santillan <afsm1995@live.com>
2020-08-04 12:48:08 -0600

    Fully Working Recursive Binary Search Implimented.

--- Sort/Sort/Model/Search Algorithims/BinarySearch.swift
+++ Sort/Sort/Model/Search Algorithims/BinarySearch.swift
@@ -10,29 +10,32 @@ import Foundation
 
 let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
 let target = 1
+var runCount = 1
 
 func binarySearch(targetArray: [Int], target: Int, frontPointer: Int, tailPointer: Int) -> Int {
     let center = Int((tailPointer - frontPointer)/2)
+    var result = -1
+    runCount += 1
+    
+    if target > array[(array.count - 1)] || target < array[0] {
+        return -1
+    }
+    
     if array[center] == target {
-        print("target", target, "index", center, "array value", array[center])
-        return target
+        return center
     } else if (array[frontPointer] == target) {
-        print("front", frontPointer, "index", frontPointer, "array value", array[frontPointer])
         return frontPointer
     } else if (array[tailPointer] == target) {
-        print("tail", tailPointer, "index", tailPointer, "array value", array[tailPointer])
         return tailPointer
     } else if array[center] > target {
-        print("explore the left", frontPointer, center, tailPointer)
-        binarySearch(targetArray: array, target: target, frontPointer: frontPointer+1, tailPointer: center-1)
+        result = binarySearch(targetArray: array, target: target, frontPointer: frontPointer+1, tailPointer: center-1)
     } else if array[center] < target {
-        print("explore the right", frontPointer, center, tailPointer)
-        binarySearch(targetArray: array, target: target, frontPointer: center+1, tailPointer: tailPointer-1)
+        result = binarySearch(targetArray: array, target: target, frontPointer: center+1, tailPointer: tailPointer-1)
     } else if ((frontPointer+1) == center) {
         return -1
     }
-    return -2
+    return result
 }
 
-let result = binarySearch(targetArray: array, target: 22, frontPointer: 0, tailPointer: (array.count)-1)
-print(result)
+//print(binarySearch(targetArray: array, target: 5, frontPointer: 0, tailPointer: (array.count)-1))
+print("runs", runCount, "result", binarySearch(targetArray: array, target: 10, frontPointer: 0, tailPointer: (array.count)-1))
