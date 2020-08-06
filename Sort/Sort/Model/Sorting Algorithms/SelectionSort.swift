ba800ee096c5d3ebffddfe41adaa9fd020f2f716
Alvaro Santillan <afsm1995@live.com>
2020-08-06 13:23:18 -0600

    New Buggy Selection Sort Implimentaion In Progress.

--- Sort/Sort/Model/Sorting Algorithms/SelectionSort.swift
+++ Sort/Sort/Model/Sorting Algorithms/SelectionSort.swift
@@ -18,60 +18,45 @@ class SelectionSort {
 
     func selectionSort(gameboard: [SkNodeAndLocation], resuming: Bool) -> [[SkNodeLocationAndColor]] {
         var swapSquareAndColor = [[SkNodeLocationAndColor]]()
+        let playableGameboard = scene.playableGameboard
         var isSorted = false
-        var currentMinimumValue = UIColor()
+        var currentMinimumValue = CGFloat()
         var currentMinimumIndex = 0
-        var playableGameboard = scene.playableGameboard
-        var tempi: SkNodeLocationAndColor
-        var tempj: SkNodeLocationAndColor
-        var min: SkNodeLocationAndColor
-        
-        var tempStructure: [UIColor] = []
-        for i in scene.gameBoard {
-            tempStructure.append(i.square.fillColor)
-        }
         
         for (iIndex, _) in playableGameboard.enumerated() {
             isSorted = true
+            var redI: CGFloat = 0
+            var greenI: CGFloat = 0
+            var blueI: CGFloat = 0
+            var alphaI: CGFloat = 0
+            var redJ: CGFloat = 0
+            var greenJ: CGFloat = 0
+            var blueJ: CGFloat = 0
+            var alphaJ: CGFloat = 0
             
-            var redOne: CGFloat = 0
-            var greenOne: CGFloat = 0
-            var blueOne: CGFloat = 0
-            var alphaOne: CGFloat = 0
-            var redTwo: CGFloat = 0
-            var greenTwo: CGFloat = 0
-            var blueTwo: CGFloat = 0
-            var alphaTwo: CGFloat = 0
-            var redThree: CGFloat = 0
-            var greenThree: CGFloat = 0
-            var blueThree: CGFloat = 0
-            var alphaThree: CGFloat = 0
-            
-            currentMinimumValue = playableGameboard[iIndex].square.fillColor
+            playableGameboard[iIndex].square.fillColor.getRed(&redI, green: &greenI, blue: &blueI, alpha: &alphaI)
+            currentMinimumValue = alphaI
             currentMinimumIndex = iIndex
             for jIndex in (iIndex...(playableGameboard.count-1)) {
-                playableGameboard[iIndex].square.fillColor = UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne)
-                playableGameboard[jIndex].square.fillColor = UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo)
-                playableGameboard[currentMinimumIndex].square.fillColor = UIColor(red: redThree, green: greenThree, blue: blueThree, alpha: alphaThree)
-                
-                tempi = SkNodeLocationAndColor(square: playableGameboard[iIndex].square, location: Tuple(x: playableGameboard[iIndex].location.y, y: playableGameboard[iIndex].location.x), color: UIColor(red: redTwo, green: greenTwo, blue: blueTwo, alpha: alphaTwo))
-                tempj = SkNodeLocationAndColor(square: playableGameboard[jIndex].square, location: Tuple(x: playableGameboard[jIndex].location.y, y: playableGameboard[jIndex].location.x), color: UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne))
-                min = SkNodeLocationAndColor(square: playableGameboard[currentMinimumIndex].square, location: Tuple(x: playableGameboard[currentMinimumIndex].location.y, y: playableGameboard[currentMinimumIndex].location.x), color: UIColor(red: redOne, green: greenOne, blue: blueOne, alpha: alphaOne))
-                
-                isSorted = false
-                
-                let jValue = playableGameboard[jIndex].square.fillColor
-                if alphaTwo < alphaThree {
-                    currentMinimumValue = playableGameboard[jIndex].square.fillColor
+                playableGameboard[jIndex].square.fillColor.getRed(&redJ, green: &greenJ, blue: &blueJ, alpha: &alphaJ)
+                let jValue = alphaJ
+                if jValue < currentMinimumValue {
+                    currentMinimumValue = alphaJ
                     currentMinimumIndex = jIndex
-                    swapSquareAndColor.append([tempi, min])
+                    
+                    let tempj = SkNodeLocationAndColor(square: playableGameboard[jIndex].square, location: Tuple(x: playableGameboard[jIndex].location.y, y: playableGameboard[jIndex].location.x), color: UIColor(red: redJ, green: greenJ, blue: blueJ, alpha: alphaJ))
+                    swapSquareAndColor.append([tempj])
                 }
             }
-            let tempIValue = playableGameboard[iIndex]
-            playableGameboard[iIndex] = playableGameboard[currentMinimumIndex]
-            playableGameboard[currentMinimumIndex] = tempIValue
+            let tempIValue = playableGameboard[iIndex].square.fillColor
+            playableGameboard[iIndex].square.fillColor = playableGameboard[currentMinimumIndex].square.fillColor
+            playableGameboard[currentMinimumIndex].square.fillColor = tempIValue
             
-            isSorted = false
+            let tempi = SkNodeLocationAndColor(square: playableGameboard[iIndex].square, location: Tuple(x: playableGameboard[iIndex].location.y, y: playableGameboard[iIndex].location.x), color: UIColor(red: redI, green: greenI, blue: blueI, alpha: alphaI))
+            
+            let tempMin = SkNodeLocationAndColor(square: playableGameboard[currentMinimumIndex].square, location: Tuple(x: playableGameboard[currentMinimumIndex].location.y, y: playableGameboard[currentMinimumIndex].location.x), color: UIColor(red: redI, green: greenI, blue: blueI, alpha: alphaI))
+            
+            swapSquareAndColor.append([tempi, tempMin])
         }
         return swapSquareAndColor
     }
