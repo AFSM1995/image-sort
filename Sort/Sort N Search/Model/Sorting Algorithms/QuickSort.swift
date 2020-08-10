9e59b8bfe6499c30e8915e542f29ab44d7e09548
Alvaro Santillan <afsm1995@live.com>
2020-08-09 23:16:59 -0600

    Quick Sort Refactor Breakpoint.

--- Sort/Sort N Search/Model/Sorting Algorithms/QuickSort.swift
+++ Sort/Sort N Search/Model/Sorting Algorithms/QuickSort.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class QuickSort {
     weak var scene: GameScene!
+    var pendingAnimations = [[SkNodeLocationAndColor]]()
     
     init(scene: GameScene) {
         self.scene = scene
@@ -37,64 +38,63 @@ class QuickSort {
         }
     }
     
-    func quickSortHelper(resuming: Bool) -> [[SkNodeLocationAndColor]] {
-        var pendingAnimations = [[SkNodeLocationAndColor]]()
+    func quickSortHelper(resuming: Bool) -> [[SkNodeLocationAndColor]]{
+        // Save the gameboard apperiance before animations.
         let initialGameboardLayout = initialGameBoardAperianceSaver()
-        let playableGameboard = scene.playableGameboard
-        
-        func quickSort(frontPointer: Int, endPointer: Int) {
-            if frontPointer == endPointer {
-                return
-            } else if endPointer < frontPointer {
-                return
-            }
-            
-            let endPointerColor = playableGameboard[endPointer].square.fillColor
-            let endPointerAlpha = playableGameboard[endPointer].square.fillColor.toComponents().alpha
-            let fullPivotColor = endPointerColor
-            var jIndex = frontPointer
-            var iIndex = frontPointer-1
-            
-            
-            for _ in (0...(endPointer-1)) {
-                if jIndex < (playableGameboard.count-1) {
-                    let jIndexColorAlpha = playableGameboard[jIndex].square.fillColor.toComponents().alpha
-                    if jIndexColorAlpha < endPointerAlpha {
-                        iIndex += 1
-                        let tempIValue = playableGameboard[iIndex].square.fillColor
-                        playableGameboard[iIndex].square.fillColor = playableGameboard[jIndex].square.fillColor
-                        playableGameboard[jIndex].square.fillColor = tempIValue
-                        
-                        let newIIndex = SkNodeLocationAndColor(square: playableGameboard[iIndex].square, location: playableGameboard[iIndex].location, color: tempIValue)
-                        let newJIndex = SkNodeLocationAndColor(square: playableGameboard[jIndex].square, location: playableGameboard[jIndex].location, color: tempIValue)
-                        pendingAnimations.append([newJIndex, newIIndex])
-                    }
-                    jIndex += 1
-                    for i in scene.playableGameboard {
-                        print(i.square.fillColor)
-                    }
-                    print("New")
-                }
-            }
-            
-            let newIPlusOneColor = SkNodeLocationAndColor(square: playableGameboard[iIndex+1].square, location: playableGameboard[iIndex+1].location, color: playableGameboard[iIndex+1].square.fillColor)
-            let newEndPointerColor = SkNodeLocationAndColor(square: playableGameboard[endPointer].square, location: playableGameboard[endPointer].location, color: playableGameboard[endPointer].square.fillColor)
-            pendingAnimations.append([newEndPointerColor, newIPlusOneColor])
-            
-            let finalPivotValue = playableGameboard[iIndex+1].square.fillColor
-            playableGameboard[iIndex+1].square.fillColor = fullPivotColor
-            playableGameboard[endPointer].square.fillColor = finalPivotValue
-            
-            if endPointer-1 != frontPointer {
-                quickSort(frontPointer: frontPointer, endPointer: iIndex)
-                quickSort(frontPointer: iIndex+2, endPointer: endPointer)
-            }
-        }
-        
+        // Run Quick sort on the graph.
         quickSort(frontPointer: 0, endPointer: scene.playableGameboard.count-1)
-        
         // Restores grid back to pre-sort apperiance before return.
         initialGameBoardAperianceRestorer(resuming: resuming, initialGameboardLayout: initialGameboardLayout)
         return pendingAnimations
+    }
+    
+    func quickSort(frontPointer: Int, endPointer: Int) {
+        let playableGameboard = scene.playableGameboard
+        
+        if frontPointer == endPointer {
+            return
+        } else if endPointer < frontPointer {
+            return
+        }
+        
+        let endPointerColor = playableGameboard[endPointer].square.fillColor
+        let endPointerAlpha = endPointerColor.toComponents().alpha
+        let fullPivotColor = endPointerColor
+        var jIndex = frontPointer
+        var iIndex = frontPointer-1
+        
+        for _ in (0...(endPointer-1)) {
+            if jIndex < (playableGameboard.count-1) {
+                let jIndexColorAlpha = playableGameboard[jIndex].square.fillColor.toComponents().alpha
+                if jIndexColorAlpha < endPointerAlpha {
+                    iIndex += 1
+                    let tempIValue = playableGameboard[iIndex].square.fillColor
+                    playableGameboard[iIndex].square.fillColor = playableGameboard[jIndex].square.fillColor
+                    playableGameboard[jIndex].square.fillColor = tempIValue
+                    
+                    let newIIndex = SkNodeLocationAndColor(square: playableGameboard[iIndex].square, location: playableGameboard[iIndex].location, color: tempIValue)
+                    let newJIndex = SkNodeLocationAndColor(square: playableGameboard[jIndex].square, location: playableGameboard[jIndex].location, color: tempIValue)
+                    pendingAnimations.append([newJIndex, newIIndex])
+                }
+                jIndex += 1
+                for i in scene.playableGameboard {
+                    print(i.square.fillColor)
+                }
+                print("New")
+            }
+        }
+        
+        let newIPlusOneColor = SkNodeLocationAndColor(square: playableGameboard[iIndex+1].square, location: playableGameboard[iIndex+1].location, color: playableGameboard[iIndex+1].square.fillColor)
+        let newEndPointerColor = SkNodeLocationAndColor(square: playableGameboard[endPointer].square, location: playableGameboard[endPointer].location, color: playableGameboard[endPointer].square.fillColor)
+        pendingAnimations.append([newEndPointerColor, newIPlusOneColor])
+        
+        let finalPivotValue = playableGameboard[iIndex+1].square.fillColor
+        playableGameboard[iIndex+1].square.fillColor = fullPivotColor
+        playableGameboard[endPointer].square.fillColor = finalPivotValue
+        
+        if endPointer-1 != frontPointer {
+            quickSort(frontPointer: frontPointer, endPointer: iIndex)
+            quickSort(frontPointer: iIndex+2, endPointer: endPointer)
+        }
     }
 }
