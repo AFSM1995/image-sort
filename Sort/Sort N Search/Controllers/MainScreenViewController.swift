554645e4d5412965903d8cc8b16c5e860ab7662e
Alvaro Santillan <afsm1995@live.com>
2020-08-14 21:13:22 -0600

    Fixed Main Screen Label Text.

--- Sort/Sort N Search/Controllers/MainScreenViewController.swift
+++ Sort/Sort N Search/Controllers/MainScreenViewController.swift
@@ -55,8 +55,8 @@ class MainScreenViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func loadUserData() {
-        highScoreLabel.text = "Swaps: \(defaults.integer(forKey: "highScore"))"
-        lastScoreLabel.text = "Comps: \(defaults.integer(forKey: "lastScore"))"
+        highScoreLabel.text = "Last Swaps: \(defaults.integer(forKey: "highScore"))"
+        lastScoreLabel.text = "Last Comps: \(defaults.integer(forKey: "lastScore"))"
         defaults.bool(forKey: "Dark Mode On Setting") ? (overrideUserInterfaceStyle = .dark) : (overrideUserInterfaceStyle = .light)
         segControl.font(name: "Dogica_Pixel", size: 9)
     }
